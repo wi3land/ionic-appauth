f24a70a4d9f3af687dbb4d50ce5e247682efac05
Matt Raible <matt.raible@okta.com>
2022-08-26 12:39:40 -0600

    Formatting

--- src/auth-browser.ts
+++ src/auth-browser.ts
@@ -1,26 +1,28 @@
 export abstract class Browser {
-    protected onCloseFunction : Function = () => {};
+  protected onCloseFunction: Function = () => {
+  };
 
-    abstract showWindow(url : string, callbackUrl?: string) : string | undefined | Promise<string | undefined>;
-    abstract closeWindow(): void | Promise<void>;
+  abstract showWindow(url: string, callbackUrl?: string): string | undefined | Promise<string | undefined>;
 
-    browserCloseListener(closeBrowserEvent : Function){
-        this.onCloseFunction = closeBrowserEvent;
-    }
+  abstract closeWindow(): void | Promise<void>;
+
+  browserCloseListener(closeBrowserEvent: Function) {
+    this.onCloseFunction = closeBrowserEvent;
+  }
 }
 
 export class DefaultBrowser extends Browser {
-    public showWindow(url: string) : string | undefined {
-        const openWindow = window.open(url, "_self")
-        if (openWindow) {
-            openWindow.addEventListener('beforeupload', () => this.onCloseFunction());
-        }
-
-        return;
+  public showWindow(url: string): string | undefined {
+    const openWindow = window.open(url, '_self')
+    if (openWindow) {
+      openWindow.addEventListener('beforeupload', () => this.onCloseFunction());
     }
 
-    public closeWindow(): void {
-        // Invoking window.close() is not desired. It will either be ignored (most of the time),
-        // or it will close the current browser tab if this site was opened via a "_blank" target.
-    }
+    return;
+  }
+
+  public closeWindow(): void {
+    // Invoking window.close() is not desired. It will either be ignored (most of the time),
+    // or it will close the current browser tab if this site was opened via a "_blank" target.
+  }
 }
